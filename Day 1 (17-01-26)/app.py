5f66b03dc83fe35df17eeccd86a38113de968117
Mutyyab <mutyyab5402@gmail.com>
2026-01-18 02:29:02 +0500

    Day 1

---
+++ Day 1 (17-01-26)/app.py
@@ -0,0 +1,44 @@
+# print('Muhammad Mutyyab')
+from jinja2.lexer import integer_re
+from numpy import number
+
+My_Name = input('Enter your name: ')
+age = input('Enter your age: ')
+# age = int(age)
+message = f'Hello, {My_Name}! You are {age} years old.'
+# print(message.upper())
+print(message.lower())
+# print(message.find('M'))
+# print(message.replace('M','MM'))
+# print('Mutyyab' in message)
+# type()
+# len()
+
+# ** (Raised to power)
+# // returns integer
+# / returns floating point number
+# + for addition
+# - for subtraction
+# * for multiplication
+# % modulus returns remainder
+
+
+# email = '''
+# Asalam u alaikum,
+#
+# Respected Sir,
+#             My name is Mutyyab and i am from Section A session 2024. It is a humble request to you
+#             to kindly allow me to sit in final exams.
+#
+# Thank you,
+#
+# Regards
+# Muhammad Mutyyab
+# '''
+
+# email = '<EMAIL>'
+# print(email[5])
+# # print(email[include:exclude])
+# print(email[1:-4])
+# print(email[2:5])
+# print(email[-4])
