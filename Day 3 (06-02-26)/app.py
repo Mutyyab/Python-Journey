02adbe3a0f85ea25bcdd757a7b7dfdbd1b97509c
Mutyyab <mutyyab5402@gmail.com>
2026-02-21 22:29:07 +0500

    Day 4

--- Day 3 (06-02-26)/app.py
+++ Day 3 (06-02-26)/app.py
@@ -1,11 +1,18 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart,Reference
+
 Book = xl.load_workbook('Book1.xlsx')
 Sheet1 = Book['Sheet1']
-Cell = Sheet1.cell(row=1, column=1)
+
 for row in range(2,Sheet1.max_row+1):
      cell = Sheet1.cell(row=row, column=3)
      corrected_price = cell.value*0.9
      corrected_price_cell = Sheet1.cell(row=row, column=4)
      corrected_price_cell.value = corrected_price
+values = Reference(Sheet1,min_row = 2, max_row = Sheet1.max_row,min_col=4,max_col=4)
 
-Book.save('Book2.xlsx')
+chart = BarChart()
+chart.add_data(values)
+Sheet1.add_chart(chart,'e2')
+
+Book.save('Book3.xlsx')
---
+++ Day 4 (21-02-26)/Email/SendEmail.py
@@ -0,0 +1,20 @@
+import smtplib as smtp
+from dotenv import load_dotenv as ld
+import os
+
+def send_email(to,from_where,message):
+
+    server = None
+    ld('data.env')
+    password = os.getenv('PASSWORD')
+    try:
+        server = smtp.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_where, password)
+        server.sendmail(from_where, to, message)
+        print("Message sent successfully")
+    except Exception as e:
+        print("Something went wrong:",e)
+    finally:
+        if server:
+            server.quit()
